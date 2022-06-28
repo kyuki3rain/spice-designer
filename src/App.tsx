0c51bfd396ae47dfb4e468c3a041f38fc2342c4a
kyuki3rain <kyuki3rain>
2022-06-29 00:43:10 +0900

    仮想座標の追加＆ズーム、移動の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import './App.css';
+import { HorizontalGrids, VerticalGrids } from './Grid';
+import { add, fix, RealPoint, sub, toFixedVirtualGrid, toRealGrid, toVirtualGrid, VirtualPoint } from './helpers/gridhelper';
 import { useWindowSize } from './useWindowSize';
 
 type LineState = {
-  points: number[];
+  points: VirtualPoint[];
   key: string;
   color: string;
 }
 
-const limit = (x: number, pitch: number) => {
-  return pitch * Math.round(x / pitch)
-}
-
-const createLine = (line: LineState) => {
+const createLine = (line: LineState, pitch: number, upperLeft: VirtualPoint) => {
   return <Line
     key={line.key}
     x={0}
     y={0}
-    points={line.points}
+    points={line.points.reduce((a, vp) => {
+      let rp = toRealGrid(vp, pitch, upperLeft);
+      return a.concat([rp.x, rp.y])
+    }, [] as number[])}
     stroke={line.color}
     strokeWidth={2}
   />
 }
 
-const createGrid = (points: number[], key: string) => {
-  return <Line
-    key={key}
-    x={0}
-    y={0}
-    points={points}
-    stroke="lightgray"
-    strokeWidth={1}
-  />
-}
 
 const modeToCursorStyle = (mode: string) => {
   switch (mode) {
@@ -42,47 +33,41 @@ const modeToCursorStyle = (mode: string) => {
   }
 }
 
-function usePrevious(value: any) {
-  const ref = useRef(null);
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
-
 const App: React.FC = () => {
   const { height, width } = useWindowSize();
-  const [pitch, setPitch] = useState(20);
-  const [lines, setLines] = useState([] as LineState[]);
-  const [selectedPoints, setSelectedPoints] = useState([] as number[]);
-  const [mode, setMode] = useState("none");
-  const prevPitch = usePrevious(pitch);
 
-  let VerticalGrids = [...Array(Math.floor(width / pitch))].map((_, i) =>
-    createGrid([i * pitch, 0, i * pitch, height], `vertical_grid_${i}`)
-  );
-  let HorizontalGrids = [...Array(Math.floor(height / pitch))].map((_, i) =>
-    createGrid([0, i * pitch, width, i * pitch], `horizontal_grid_${i}`)
-  );
+  const [pitch, setPitch] = useState(20);
+  const [upperLeft, setUpperLeft] = useState({ vx: 0, vy: 0 });
+  const [lines, setLines] = useState([] as LineState[]);
+  const [selectedPoints, setSelectedPoints] = useState([] as VirtualPoint[]);
+  const [mode, setMode] = useState("none");
 
   useEffect(() => {
+    setLines(lines.slice(0, -1).concat({ points: selectedPoints, key: `line_${lines.length}`, color: "black" }));
     setSelectedPoints([]);
   }, [mode]);
 
-  useEffect(() => {
-    if (prevPitch)
-      setLines(lines.map(l => { return { ...l, points: l.points.map(p => p / prevPitch * pitch) } }))
-  }, [pitch]);
-
   const onWheel = (e: any) => {
     e.preventDefault();
-    let new_pitch = pitch;
 
-    if (e.deltaY < 0) new_pitch++; // Zoom in
-    else new_pitch--; // Zoom out
+    if (e.ctrlKey) {
+      let pos: RealPoint = { x: e.clientX, y: e.clientY };
+      let raw_vpos = toVirtualGrid(pos, pitch, upperLeft);
 
-    // Restrict scale
-    setPitch(Math.min(Math.max(5, new_pitch), 50));
+      let new_pitch = pitch;
+      if (e.deltaY < 0) new_pitch++; // Zoom in
+      else new_pitch--; // Zoom out
+
+      new_pitch = Math.min(Math.max(5, new_pitch), 50);
+      let new_raw_vpos = toVirtualGrid(pos, new_pitch, upperLeft);
+
+      setPitch(new_pitch);
+      setUpperLeft(add(upperLeft, sub(raw_vpos, new_raw_vpos)));
+    } else if (e.shiftKey) {
+      setUpperLeft(add(upperLeft, toVirtualGrid({ x: e.deltaY, y: e.deltaX }, pitch, { vx: 0, vy: 0 })));
+    } else {
+      setUpperLeft(add(upperLeft, toVirtualGrid({ x: e.deltaX, y: e.deltaY }, pitch, { vx: 0, vy: 0 })));
+    }
   };
   const divRef = useRef(null);
 
@@ -97,7 +82,6 @@ const App: React.FC = () => {
 
   return (
     <div ref={divRef} tabIndex={1} onKeyDown={e => {
-      console.log(e.code);
       switch (e.code) {
         case 'Escape':
           setMode("none");
@@ -118,9 +102,9 @@ const App: React.FC = () => {
         switch (mode) {
           case "line":
             let pos = e.target.getRelativePointerPosition();
-            console.log(pos);
+            let vpos = toFixedVirtualGrid(pos, pitch, upperLeft);
 
-            let newSelectedPoints = [...selectedPoints, limit(pos.x, pitch), limit(pos.y, pitch)];
+            let newSelectedPoints = [...selectedPoints, vpos];
             if (selectedPoints.length) {
               setLines(lines.slice(0, -1).concat({ points: newSelectedPoints, key: `line_${lines.length}`, color: "black" }));
             }
@@ -134,18 +118,17 @@ const App: React.FC = () => {
           case "line":
             if (!selectedPoints.length) break;
 
-            let pos = e.target.getRelativePointerPosition();
-            console.log(pos);
-            let newSelectedPoints = [...selectedPoints, limit(pos.x, pitch), limit(pos.y, pitch)];
+            let vpos = toFixedVirtualGrid(e.target.getRelativePointerPosition(), pitch, upperLeft);
+            let newSelectedPoints = [...selectedPoints, vpos];
             setLines(lines.slice(0, -1).concat({ points: newSelectedPoints, key: `line_${lines.length}`, color: "black" }));
             break;
           default:
         }
       }}>
         <Layer>
-          {VerticalGrids}
-          {HorizontalGrids}
-          {lines.map(l => createLine(l))}
+          {VerticalGrids(pitch, width, height, upperLeft.vx)}
+          {HorizontalGrids(pitch, width, height, upperLeft.vy)}
+          {lines.map(l => createLine(l, pitch, upperLeft))}
         </Layer>
       </Stage>
     </div >
