2ed605fa3cf1610d19631525e9c556f2e4834003
kyuki3rain <kyuki3rain>
2022-06-28 22:53:06 +0900

    add zoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import './App.css';
 import { useWindowSize } from './useWindowSize';
+
+type LineState = {
+  points: number[];
+  key: string;
+  color: string;
+}
 
 const limit = (x: number, pitch: number) => {
   return pitch * Math.round(x / pitch)
 }
 
-const createLine = (points: number[], key: string, color: string) => {
+const createLine = (line: LineState) => {
   return <Line
-    key={key}
+    key={line.key}
     x={0}
     y={0}
-    points={points}
-    stroke={color}
+    points={line.points}
+    stroke={line.color}
     strokeWidth={2}
   />
 }
@@ -36,12 +42,21 @@ const modeToCursorStyle = (mode: string) => {
   }
 }
 
+function usePrevious(value: any) {
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 const App: React.FC = () => {
   const { height, width } = useWindowSize();
   const [pitch, setPitch] = useState(20);
-  const [lines, setLines] = useState([] as JSX.Element[]);
+  const [lines, setLines] = useState([] as LineState[]);
   const [selectedPoints, setSelectedPoints] = useState([] as number[]);
   const [mode, setMode] = useState("none");
+  const prevPitch = usePrevious(pitch);
 
   let VerticalGrids = [...Array(Math.floor(width / pitch))].map((_, i) =>
     createGrid([i * pitch, 0, i * pitch, height], `vertical_grid_${i}`)
@@ -50,8 +65,38 @@ const App: React.FC = () => {
     createGrid([0, i * pitch, width, i * pitch], `horizontal_grid_${i}`)
   );
 
+  useEffect(() => {
+    setSelectedPoints([]);
+  }, [mode]);
+
+  useEffect(() => {
+    if (prevPitch)
+      setLines(lines.map(l => { return { ...l, points: l.points.map(p => p / prevPitch * pitch) } }))
+  }, [pitch]);
+
+  const onWheel = (e: any) => {
+    e.preventDefault();
+    let new_pitch = pitch;
+
+    if (e.deltaY < 0) new_pitch++; // Zoom in
+    else new_pitch--; // Zoom out
+
+    // Restrict scale
+    setPitch(Math.min(Math.max(5, new_pitch), 50));
+  };
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    // @ts-ignore
+    divRef.current?.addEventListener("wheel", onWheel, { passive: false });
+    return (() => {
+      // @ts-ignore
+      divRef.current?.removeEventListener("wheel", onWheel, { passive: false });
+    });
+  });
+
   return (
-    <div tabIndex={1} onKeyDown={e => {
+    <div ref={divRef} tabIndex={1} onKeyDown={e => {
       console.log(e.code);
       switch (e.code) {
         case 'Escape':
@@ -59,6 +104,12 @@ const App: React.FC = () => {
           break;
         case 'KeyL':
           setMode("line");
+          break;
+        case 'KeyE':
+          setPitch(pitch + 1);
+          break;
+        case 'KeyR':
+          setPitch(pitch - 1);
           break;
         default:
       }
@@ -71,7 +122,7 @@ const App: React.FC = () => {
 
             let newSelectedPoints = [...selectedPoints, limit(pos.x, pitch), limit(pos.y, pitch)];
             if (selectedPoints.length) {
-              setLines(lines.slice(0, -1).concat(createLine(newSelectedPoints, `line_${lines.length}`, "black")));
+              setLines(lines.slice(0, -1).concat({ points: newSelectedPoints, key: `line_${lines.length}`, color: "black" }));
             }
 
             setSelectedPoints(newSelectedPoints);
@@ -86,7 +137,7 @@ const App: React.FC = () => {
             let pos = e.target.getRelativePointerPosition();
             console.log(pos);
             let newSelectedPoints = [...selectedPoints, limit(pos.x, pitch), limit(pos.y, pitch)];
-            setLines(lines.slice(0, -1).concat(createLine(newSelectedPoints, `line_${lines.length}`, "black")));
+            setLines(lines.slice(0, -1).concat({ points: newSelectedPoints, key: `line_${lines.length}`, color: "black" }));
             break;
           default:
         }
@@ -94,7 +145,7 @@ const App: React.FC = () => {
         <Layer>
           {VerticalGrids}
           {HorizontalGrids}
-          {lines}
+          {lines.map(l => createLine(l))}
         </Layer>
       </Stage>
     </div >
