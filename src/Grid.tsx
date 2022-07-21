498b3bbd4a98aab4876d21d7b03676253a11609b
kyuki3rain <kyuki3rain>
2022-07-21 11:39:47 +0900

    gridをコンポーネント化

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,18 +1,36 @@
-import { Line } from 'react-konva';
+import { Group, Line } from 'react-konva';
+import { useRecoilValue } from 'recoil';
+import { pitchAtom, upperLeftAtom } from './atoms';
+import { useWindowSize } from './hooks/useWindowSize';
 
 const createGrid = (points: number[], key: string) => (
   <Line key={key} x={0} y={0} points={points} stroke="lightgray" strokeWidth={1} />
 );
 
-export const VerticalGrids = (pitch: number, width: number, height: number, upperLeftX: number) => {
-  const hosei = Math.ceil(upperLeftX) - upperLeftX;
-  return [...(Array(Math.ceil(width / pitch)) as number[])].map((_, i) =>
-    createGrid([(i + hosei) * pitch, 0, (i + hosei) * pitch, height], `vertical_grid_${i}`)
+const Grid: React.FC = () => {
+  const { height, width } = useWindowSize();
+  const pitch = useRecoilValue(pitchAtom);
+  const upperLeft = useRecoilValue(upperLeftAtom);
+
+  const verticalCorrection = Math.ceil(upperLeft.vx) - upperLeft.vx;
+  const horizontalCorrection = Math.ceil(upperLeft.vy) - upperLeft.vy;
+
+  return (
+    <Group>
+      {[...(Array(Math.ceil(width / pitch)) as number[])].map((_, i) =>
+        createGrid(
+          [(i + verticalCorrection) * pitch, 0, (i + verticalCorrection) * pitch, height],
+          `vertical_grid_${i}`
+        )
+      )}
+      {[...(Array(Math.floor(height / pitch)) as number[])].map((_, i) =>
+        createGrid(
+          [0, (i + horizontalCorrection) * pitch, width, (i + horizontalCorrection) * pitch],
+          `horizontal_grid_${i}`
+        )
+      )}
+    </Group>
   );
 };
-export const HorizontalGrids = (pitch: number, width: number, height: number, upperLeftY: number) => {
-  const hosei = Math.ceil(upperLeftY) - upperLeftY;
-  return [...(Array(Math.floor(height / pitch)) as number[])].map((_, i) =>
-    createGrid([0, (i + hosei) * pitch, width, (i + hosei) * pitch], `horizontal_grid_${i}`)
-  );
-};
+
+export default Grid;
