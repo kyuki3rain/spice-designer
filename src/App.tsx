8bc1355be8a057239b49c04e09022b4135944cc9
kyuki3rain <kyuki3rain>
2022-07-22 10:47:44 +0900

    モードとデバッグ情報のボタンができた

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { RecoilRoot } from 'recoil';
 import DrawArea from './DrawArea';
+import ButtonArea from './ButtonArea';
 
 const App: React.FC = () => (
   <React.StrictMode>
     <RecoilRoot>
+      <ButtonArea />
       <DrawArea />
     </RecoilRoot>
   </React.StrictMode>
---
+++ src/ButtonArea.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import { Fab } from '@mui/material';
+import React from 'react';
+import { useRecoilCallback, useSetRecoilState } from 'recoil';
+import { Add, Description, HorizontalRule } from '@mui/icons-material';
+import { edgeListAtom, modeAtom, nodeListAtom } from './atoms';
+import { Mode } from './helpers/modehelper';
+
+const ButtonArea: React.FC = () => {
+  const setMode = useSetRecoilState(modeAtom);
+  const showInfo = useRecoilCallback(
+    ({ snapshot }) =>
+      () => {
+        const nodeList = snapshot.getLoadable(nodeListAtom).getValue();
+        const edgeList = snapshot.getLoadable(edgeListAtom).getValue();
+        console.log('nodeList: ', nodeList);
+        console.log('edgeList: ', edgeList);
+      },
+    []
+  );
+
+  return (
+    <div style={{ float: 'left' }}>
+      <Fab aria-label="wire" onClick={() => setMode(Mode.WIRE)}>
+        <HorizontalRule />
+      </Fab>
+      <Fab aria-label="symbol" color="primary" onClick={() => setMode(Mode.SYMBOL)}>
+        <Add />
+      </Fab>
+      <Fab color="primary" aria-label="get log" onClick={showInfo}>
+        <Description />
+      </Fab>
+    </div>
+  );
+};
+
+export default ButtonArea;
