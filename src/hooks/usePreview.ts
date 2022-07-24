e3390aecd1bfedc9f02b7a62324593f9f392cf39
kyuki3rain <kyuki3rain>
2022-07-25 01:05:23 +0900

    usePreview内でmodeごとの差分を吸収

--- src/hooks/usePreview.ts
+++ src/hooks/usePreview.ts
@@ -2,37 +2,46 @@ import { useCallback } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { previewPointAtom, previewSymbolAtom, selectedNodeIdAtom, symbolTypeAtom } from '../atoms';
 import { VirtualPoint } from '../helpers/gridhelper';
+import { Mode, ModeType } from '../helpers/modehelper';
 import { SymbolTypes } from '../symbols';
 
 export const usePreview = () => {
   const setSelectedNodeId = useSetRecoilState(selectedNodeIdAtom);
   const setPreviewPoint = useSetRecoilState(previewPointAtom);
   const setSymbolType = useSetRecoilState(symbolTypeAtom);
-  const setPreviewRawSymbol = useSetRecoilState(previewSymbolAtom);
+  const setPreviewSymbol = useSetRecoilState(previewSymbolAtom);
   const symbolType = useRecoilValue(symbolTypeAtom);
 
-  const resetPreviewWire = useCallback(() => {
-    setSelectedNodeId(null);
-    setPreviewPoint({} as VirtualPoint);
+  const resetPreview = useCallback((mode: ModeType) => {
+    switch (mode) {
+      case Mode.WIRE:
+        setSelectedNodeId(null);
+        setPreviewPoint({} as VirtualPoint);
+        break;
+      case Mode.SYMBOL:
+        setSymbolType(SymbolTypes.CELL);
+        setPreviewSymbol(null);
+        break;
+      default:
+    }
   }, []);
 
-  const resetPreviewSymbol = useCallback(() => {
-    setSymbolType(SymbolTypes.CELL);
-    setPreviewRawSymbol(null);
-  }, []);
-
-  const setPreviewWire = useCallback((point: VirtualPoint) => {
-    setPreviewPoint(point);
-  }, []);
-
-  const setPreviewSymbol = useCallback(
-    (point: VirtualPoint) => {
-      setPreviewRawSymbol({ type: symbolType, point, key: `symbol_preview` });
+  const setPreview = useCallback(
+    (mode: ModeType, point: VirtualPoint) => {
+      switch (mode) {
+        case Mode.WIRE:
+          setPreviewPoint(point);
+          break;
+        case Mode.SYMBOL:
+          setPreviewSymbol({ type: symbolType, point, key: `symbol_preview` });
+          break;
+        default:
+      }
     },
     [symbolType]
   );
 
-  return { setPreviewWire, resetPreviewWire, setPreviewSymbol, resetPreviewSymbol };
+  return { setPreview, resetPreview };
 };
 
 export default usePreview;
