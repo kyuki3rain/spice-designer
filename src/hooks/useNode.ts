db7b2910b41b58463a89a6f0f46c54a766ecfb8b
kyuki3rain <kyuki3rain>
2022-07-26 00:13:20 +0900

    bugfix: 分割が発生する処理を線分上に限定する

--- src/hooks/useNode.ts
+++ src/hooks/useNode.ts
@@ -8,10 +8,14 @@ import { useEdge } from './useEdge';
 const isOnEdge = (a: VirtualPoint, b: VirtualPoint, c: VirtualPoint) => {
   if (a === c || b === c) return true;
 
-  if (b.vx === a.vx) return c.vx === a.vx;
-  if (b.vy === a.vy) return c.vy === a.vy;
+  if (b.vx === a.vx) return c.vx === a.vx && (c.vy - a.vy) / (b.vy - a.vy) > 0 && (c.vy - a.vy) / (b.vy - a.vy) < 1;
+  if (b.vy === a.vy) return c.vy === a.vy && (c.vx - a.vx) / (b.vx - a.vx) > 0 && (c.vx - a.vx) / (b.vx - a.vx) < 1;
 
-  return (c.vx - a.vx) / (b.vx - a.vx) === (c.vy - a.vy) / (b.vy - a.vy);
+  return (
+    (c.vx - a.vx) / (b.vx - a.vx) > 0 &&
+    (c.vx - a.vx) / (b.vx - a.vx) < 1 &&
+    (c.vx - a.vx) / (b.vx - a.vx) === (c.vy - a.vy) / (b.vy - a.vy)
+  );
 };
 
 export const useNode = () => {
